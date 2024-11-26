ec63266620be14e75b5d093476ec5c908e558bb2
99ninew <145371205+99ninew@users.noreply.github.com>
2024-11-26 18:00:33 +0800

    Material initialization

--- src/core/Material.cpp
+++ src/core/Material.cpp
@@ -1,17 +1,6 @@
 #include "Material.h"
 
-Material::Material(){
-	this->type = MaterialType::Diffuse;
-	this->twoSided = false;
-	this->diffuseReflect = glm::vec3(0.0f);
-	this->specularReflect = glm::vec3(0.0f);
-	this->alpha = 0.0f;
-	this->int_ior = 0.0f;
-	this->ext_ior = 0.0f;
-	this->eta = glm::vec3(0.0f);
-	this->k = glm::vec3(0.0f);
-	this->nonlinear = false;
-}
+Material::Material(){}
 Material::~Material() {}
 
 
--- src/core/Material.h
+++ src/core/Material.h
@@ -17,8 +17,19 @@ enum class MaterialType {
 
 class Material {
 public:
-    Material();
-    ~Material();
+    Material() {
+        this->type = MaterialType::Diffuse;
+        this->twoSided = false;
+        this->diffuseReflect = glm::vec3(0.0f);
+        this->specularReflect = glm::vec3(0.0f);
+        this->alpha = 0.0f;
+        this->int_ior = 0.0f;
+        this->ext_ior = 0.0f;
+        this->eta = glm::vec3(0.0f);
+        this->k = glm::vec3(0.0f);
+        this->nonlinear = false;
+    }
+    ~Material() {};
 
     MaterialType type;
 
