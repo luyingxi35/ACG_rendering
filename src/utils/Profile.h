df3292365f78f10186fca8a902164582e77afa8b
luyingxi35 <lu-yx23@mails.tsinghua.edu.cn>
2024-12-13 14:06:36 +0800

    add profile.h/,cpp

---
+++ src/utils/Profile.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <chrono>
+#include <string>
+
+#define PROFILE(name) Profile __profile(name);
+
+struct Profile {
+	Profile(const std::string& name);
+	~Profile();
+
+	std::string name;
+	std::chrono::high_resolution_clock::time_point start;
+};
+
